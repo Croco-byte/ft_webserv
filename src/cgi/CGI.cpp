568c9ce3487d694f26a77a765f18aabbca2bdfdb
Votre Nom <Vous@exemple.com>
2021-06-07 09:28:41 +0200

    send

--- src/cgi/CGI.cpp
+++ src/cgi/CGI.cpp
@@ -2,126 +2,102 @@
 
 CGI::CGI()
 {
-	_stdin = dup(STDIN);
-	_stdout = dup(STDOUT);
+
 }
 
 CGI::CGI(const CGI &x)
 {
-	(void)x;
+(void)x;
 }
 
 CGI::~CGI()
 {
-	
+
 }
 
-void	CGI::setBinary(std::string path)
+void		 CGI::setBinary(std::string path)
 {
-	_binary = path;
+_binary = path;
 }
 
-void	CGI::setInput(std::string content)
+void		 CGI::setInput(std::string content)
 {
-	_input = content;
+_input = content;
 }
 
-int		CGI::minipipe(void)
+void		 CGI::execute(std::string target)
 {
-	pid_t		pid;
-	int			pipefd[2];
+	pid_t					pid;
+	int						ex_fd[2];
+	int						output_fd;
+	int						input_fd;
+	char					tmp[1024];
+	int						ret;
+	std::string				output;
 
-	pipe(pipefd);
+	ex_fd[0] = dup(STDIN_FILENO);
+	ex_fd[1] = dup(STDOUT_FILENO);
 	pid = fork();
-	if (pid == 0)
+	if (pid == -1)
 	{
-		dup2(pipefd[0], STDIN_FILENO);
-		dup2(pipefd[1], STDOUT_FILENO);
-		_pipout = pipefd[1];
-		_pipin = pipefd[0];
-		return (2);
+		Console::error("Fork failed for CGI : PID = -1");
+		return ;
+	}
+	else if (pid == 0)		 // On est dans le fils
+	{
+		char **av = new char * [3];
+		av[0] = new char [_binary.length() + 1];
+		av[1] = new char [target.length() + 1];
+
+		input_fd = open("tmp/.cgi_input", O_RDWR, 0777);
+		output_fd = open("tmp/.cgi_output", O_RDWR | O_TRUNC, 0777);
+
+		dup2(output_fd, STDOUT_FILENO);
+		close(output_fd);
+		dup2(input_fd, STDIN_FILENO);
+		close(input_fd);
+
+		strcpy(av[0], _binary.c_str());
+		strcpy(av[1], target.c_str());
+		av[2] = NULL;
+		execve(_binary.c_str(), av, this->doubleStringToChar(_metaVariables));
 	}
 	else
 	{
-		_pipin = pipefd[0];
-		_pipout = pipefd[1];
-		return (1);
-	}
-}
+		int				  status;
 
-void	CGI::execute(std::string target)
-{
-	int ret(0);
-	int pipe = minipipe();
-	if (pipe == 2)
-	{
-		pid_t	pid;
+		input_fd = open("tmp/.cgi_input", O_RDWR | O_TRUNC, 0777);
+		output_fd = open("tmp/.cgi_output", O_RDWR, 0777);
 
-		pid = fork();
-		if (pid == 0)
-		{
-			if (_input.empty())
-			{
-				std::cerr << "EMPTY" << std::endl;
-				_input = " ";
-				this->addMetaVariable("CONTENT_LENGTH", "1");
-			}
-			std::cerr << "Hi :)" << std::endl;
-			if (_input.length() > 100000)
-			{
-				write(_pipout, (_input.substr(0, 1000)).c_str(), 1000);
-				this->addMetaVariable("CONTENT_LENGTH", "1000");
-			}
-			else
-				write(_pipout, _input.c_str(), _input.length());
-			std::cerr << "hello :(" << std::endl;
-			close(_pipout);
-			char **av = new char * [3];
-			av[0] = new char [_binary.length() + 1];
-			av[1] = new char [target.length() + 1];
-			strcpy(av[0], _binary.c_str());
-			strcpy(av[1], target.c_str());
-			av[2] = NULL;
-			execve(_binary.c_str(), av, this->doubleStringToChar(_metaVariables));
-			exit(1);
-		}
-		else
-			waitpid(-1, &ret, 0);
-		std::cerr << "hello" << std::endl;
-		exit(0);
-	}
-	else
-	{
-		char		tmp[RECV_SIZE];
-		std::cout << "Hanging" << std::endl;
-		waitpid(-1, &ret, 0);
-		std::cout << "bruh" << std::endl;
-		close(_pipout);
-		while ((ret = read(_pipin, tmp, RECV_SIZE - 1)) != 0)
+		write(input_fd, _input.c_str(), _input.length());
+
+		waitpid(pid, &status, 0);
+
+		while ((ret = read(output_fd, tmp, 1023)) != 0)
 		{
 			tmp[ret] = '\0';
-			_output += std::string(tmp);
-			std::cerr << "Read " << ret << " bytes." << std::endl;
+			output += std::string(tmp);
 		}
+		_output = output;
+
+		close(output_fd);
+		close(input_fd);
+
+		dup2(ex_fd[0], STDIN_FILENO);
+		dup2(ex_fd[1], STDOUT_FILENO);
 	}
-	dup2(_stdin, STDIN_FILENO);
-	dup2(_stdout, STDOUT_FILENO);
-	close(_pipout);
-	close(_pipin);
 }
 
-/*		 */
-
-void	CGI::addMetaVariable(std::string name, std::string value)
+void		 CGI::addMetaVariable(std::string name, std::string value)
 {
 	_metaVariables[name] = value;
 }
 
-char	**CGI::doubleStringToChar(DoubleString param)
+char		 **CGI::doubleStringToChar(DoubleString param)
 {
-	char		**ret;
-	std::string	tmp;
-	size_t		i;
+	char				  **ret;
+	std::string		 tmp;
+	size_t				  i;
 
 	i = 0;
 	ret = new char* [param.size() + 1];
@@ -129,27 +105,21 @@ char	**CGI::doubleStringToChar(DoubleString param)
 	{
 		tmp = it->first + "=" + it->second;
 		ret[i] = new char [tmp.length() + 1];
-		ret[i] = strcpy(ret[i], (const char *)tmp.c_str());
+		strcpy(ret[i], tmp.c_str());
 		i++;
 	}
 	ret[i] = NULL;
-	i = 0;
-	while (ret[i])
-	{
-		std::cerr << "[DEBUG] Got these env variable : " << ret[i] << std::endl;
-		i++;
-	}
 	return (ret);
 }
 
-std::string		CGI::getOutput()
+std::string				  CGI::getOutput()
 {
 	return (_output);
 }
 
-void			CGI::convertHeadersToMetaVariables(Request request)
+void						   CGI::convertHeadersToMetaVariables(Request request)
 {
-	DoubleString	headers = request.getHeaders();
+	DoubleString		 headers = request.getHeaders();
 	for (DoubleString::iterator it = headers.begin(); it != headers.end(); it++)
 	{
 		std::string name = it->first;
--- www/cgi/index.php
+++ www/cgi/index.php
@@ -1,6 +1,8 @@
 <?php
+
 header("test: abcdef");
 
+echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>'; 
 var_dump($_SERVER);
 
 echo "POST : ";
